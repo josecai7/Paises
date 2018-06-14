b980f88adae28fbd6aa574447dc55b2b6a42910c
Jose Antonio Soler Jimenez <jasoljim92@gmail.com>
2018-06-14 09:40:34 +0200

    Tierras con el backend medio creado

---
+++ Tierras/Tierras.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tierras.Domain;
+
+namespace Tierras.Backend.Models
+{
+    public class LocalDataContext: DataContext
+    {
+
+    }
+}
--- Tierras/Tierras.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Tierras.Domain
-{
-    public class Class1
-    {
-    }
-}
